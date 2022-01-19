59722b2e21e0059c4c42f86fbb9411bb2d17727b
Ed Carlos <edcarlos.moraes@outlook.com>
2022-01-19 20:19:15 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,11 +7,11 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                    <h1>Montréal Movie</h1>
+                    <a href="https://github.com/edcarlosmoraes" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/edcarlosmoraes</p>
                         </div>
                     </a>
                 </div>
